ec17b200a6be1cdb836274966a38797289f2efde
itsuki <itsuki.enjyoy@gmail.com>
2025-10-16 10:40:34 +0900

    Initial Commit

---
+++ WebviewJsCommunicationDemo/WebviewJsCommunicationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewJsCommunicationDemoApp.swift
+//  WebviewJsCommunicationDemo
+//
+//  Created by Itsuki on 2025/10/16.
+//
+
+import SwiftUI
+
+@main
+struct WebviewJsCommunicationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
